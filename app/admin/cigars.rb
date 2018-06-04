18aecf8759f7ea5c3b5762d591874dab7f5e4d1c
Raghu Betina <raghu@firstdraft.com>
2018-06-04 18:24:44 +0000

    Whitelist columns in Cigar for ActiveAdmin

--- app/admin/cigars.rb
+++ app/admin/cigars.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Cigar do
+
+ permit_params :profile, :price, :origin, :image_url
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
